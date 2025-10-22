dbb5d85d05df253c60195afe730bbccb052019e5
Patrick Norrick <pcnorrick1@gmail.com>
2025-10-21 22:34:06 -0400

    change snippet expand keys

--- nvim/lua/plugins/latex.lua
+++ nvim/lua/plugins/latex.lua
@@ -1,4 +1,4 @@
-vim.g.vimtex_view_method = "sioyek" 
+vim.g.vimtex_view_method = "sioyek"
 vim.g.vimtex_compiler_method = "latexmk"
 vim.g.vimtex_compiler_progname = "nvr"
 vim.g.vimtex_quickfix_mode = 0  -- cleaner quickfix
@@ -6,9 +6,9 @@ vim.g.vimtex_mappings_enabled = 1
 vim.g.vimtex_syntax_enabled = 0 -- gives syntax highlighting to treesitter
 
 -- UltiSnips settings (for LaTeX snippets)
-vim.g.UltiSnipsExpandTrigger = "<C-j>"
-vim.g.UltiSnipsJumpForwardTrigger  = "<C-j>"
-vim.g.UltiSnipsJumpBackwardTrigger = "<C-k>"
+vim.g.UltiSnipsExpandTrigger = "<tab>"
+vim.g.UltiSnipsJumpForwardTrigger  = "<tab>"
+vim.g.UltiSnipsJumpBackwardTrigger = "<s-tab>"
 
 -- Point UltiSnips to your snippet directories (we'll clone Gilles Castel below)
 -- First entry is the one we’ll clone.
